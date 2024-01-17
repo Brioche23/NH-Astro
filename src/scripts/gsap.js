b12d3f8b1cf7dfa752e52f5c02f43e569068d1db
Alessio Brioschi <77493369+Brioche23@users.noreply.github.com>
2024-01-17 18:00:28 +0200

    added intro animation

--- src/scripts/gsap.js
+++ src/scripts/gsap.js
@@ -1,4 +1,10 @@
 gsap.registerPlugin(ScrollTrigger);
+import SplitType from "split-type";
+
+const ourText = new SplitType(".split", { types: "lines, chars" });
+const chars = ourText.chars;
+const secondSplit = new SplitType(".char", { types: "chars" });
+const chars2 = secondSplit.chars;
 
 // create a timeline
 let tl = gsap.timeline();
@@ -8,9 +14,30 @@ let mm = gsap.matchMedia();
 tl.fromTo(
   ".intro-text",
   { opacity: 0 },
-  { opacity: 1, delay: 0.5, duration: 2, stagger: 1 }
-);
-
+  { opacity: 1, delay: 0, duration: 2, stagger: 0.5, ease: "ease-in" }
+).from(chars2, {
+  yPercent: 100,
+  opacity: 0,
+  stagger: 0.01,
+  duration: 1,
+  ease: "ease-in",
+  toggleActions: "restart none none reverse",
+  // onComplete: () => {
+  //   SplitType.revert(".char");
+  //   SplitType.revert(".split");
+  // },
+  // scrollTrigger: {
+  //   trigger: ".content",
+  //   start: "top center",
+  //   end: "bottom end",
+  //   markers: true,
+  //   scrub: 1,
+  //   onLeave: () => {
+  //     SplitType.revert(".char");
+  //     SplitType.revert(".split");
+  //   },
+  // },
+});
 gsap.utils.toArray(".block").forEach((block, i) => {
   tl.from(block, {
     scrollTrigger: {
@@ -19,6 +46,10 @@ gsap.utils.toArray(".block").forEach((block, i) => {
       pin: true,
       scrub: 1,
       markers: false,
+      onLeave: () => {
+        SplitType.revert(".char");
+        SplitType.revert(".split");
+      },
     },
   });
 });
--- src/scripts/vimeoCards.js
+++ src/scripts/vimeoCards.js
@@ -1,54 +1,57 @@
-// const playButton = this.querySelector("button.play-button");
-// const muteButton = this.querySelector("button.mute-button");
+console.log("hello");
+const videoCards = document.querySelectorAll("li.video-card");
+const infoPanels = document.querySelectorAll(".info-panel");
+const vimeoTitles = document.querySelectorAll(".vimeo-title");
+// Handle clicks on each button.
+let prevIndex = "";
 
-// const vimeoContent = this.querySelector("div.video-wrapper");
-// const videoCards = document.querySelectorAll("li.video-card");
-// const infoPanels = document.querySelectorAll(".info-panel");
-// const vimeoTitles = document.querySelectorAll(".vimeo-title");
-// // Handle clicks on each button.
+videoCards.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    toggleOpacity(videoCards, index);
+    toggleInfo(infoPanels, index);
+    toggleTitle(vimeoTitles, index);
 
-// this.addEventListener("click", () => {
-//   isPlaying ? player.pause() : player.play();
-//   isPlaying = !isPlaying;
-//   toggleOpacity(videoCards, index);
-//   toggleInfo(infoPanels, index);
-//   toggleTitle(vimeoTitles, index);
-// });
+    console.log("Prev Index:" + prevIndex);
+    console.log("Index:" + index);
 
-// function toggleOpacity(videoCards, index) {
-//   videoCards.forEach((card, i) => {
-//     if (i !== index) {
-//       card.style.width = card.style.width === "100%" ? "" : "";
+    prevIndex = index;
+  });
+});
 
-//       if (prevIndex == index) {
-//         card.classList.toggle("opacity-50");
-//         console.log("true");
-//       } else card.classList.add("opacity-50");
-//     } else {
-//       card.style.width = card.style.width === "100%" ? "" : "100%";
-//       card.classList.remove("opacity-50");
-//       card.classList.add("opacity-100");
-//     }
-//   });
-// }
+function toggleOpacity(videoCards, index) {
+  videoCards.forEach((card, i) => {
+    if (i !== index) {
+      card.style.width = card.style.width === "100%" ? "" : "";
 
-// function toggleInfo(infoPanels, index) {
-//   infoPanels.forEach((panel, i) => {
-//     if (i !== index) {
-//       panel.classList.add("grid-rows-[0fr]");
-//       panel.classList.remove("grid-rows-[1fr]");
-//     } else {
-//       panel.classList.toggle("grid-rows-[0fr]");
-//       panel.classList.toggle("grid-rows-[1fr]");
-//     }
-//   });
-// }
-// function toggleTitle(vimeoTitles, index) {
-//   vimeoTitles.forEach((title, i) => {
-//     if (i !== index) {
-//       title.classList.remove("opacity-0");
-//     } else {
-//       title.classList.toggle("opacity-0");
-//     }
-//   });
-// }
+      if (prevIndex == index) {
+        card.classList.toggle("opacity-50");
+        console.log("true");
+      } else card.classList.add("opacity-50");
+    } else {
+      card.style.width = card.style.width === "100%" ? "" : "100%";
+      card.classList.remove("opacity-50");
+      card.classList.add("opacity-100");
+    }
+  });
+}
+
+function toggleInfo(infoPanels, index) {
+  infoPanels.forEach((panel, i) => {
+    if (i !== index) {
+      panel.classList.add("grid-rows-[0fr]");
+      panel.classList.remove("grid-rows-[1fr]");
+    } else {
+      panel.classList.toggle("grid-rows-[0fr]");
+      panel.classList.toggle("grid-rows-[1fr]");
+    }
+  });
+}
+function toggleTitle(vimeoTitles, index) {
+  vimeoTitles.forEach((title, i) => {
+    if (i !== index) {
+      title.classList.remove("opacity-0");
+    } else {
+      title.classList.toggle("opacity-0");
+    }
+  });
+}
