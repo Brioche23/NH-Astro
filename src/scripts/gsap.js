4108e99c6587a5d18517d5f8dc7f972296c88f92
Alessio Brioschi <77493369+Brioche23@users.noreply.github.com>
2024-01-31 13:54:44 +0200

    Fixed opacità freccine

--- src/scripts/gsap.js
+++ src/scripts/gsap.js
@@ -1,58 +1,70 @@
 gsap.registerPlugin(ScrollTrigger);
 import SplitType from "split-type";
 
-const ourText = new SplitType(".split", { types: "lines, chars" });
-const chars = ourText.chars;
-const secondSplit = new SplitType(".char", { types: "chars" });
-const chars2 = secondSplit.chars;
+// const ourText = new SplitType(".split", { types: "lines, chars" });
+// const chars = ourText.chars;
+// const secondSplit = new SplitType(".char", { types: "chars" });
+// const chars2 = secondSplit.chars;
 
 // create a timeline
 let tl = gsap.timeline();
 
 let mm = gsap.matchMedia();
 
-tl.fromTo(
-  ".intro-text",
-  { opacity: 0 },
-  { opacity: 1, delay: 0, duration: 2, stagger: 0.5, ease: "ease-in" }
-).from(chars2, {
-  yPercent: 100,
-  opacity: 0,
-  stagger: 0.01,
-  duration: 1,
-  ease: "ease-in",
-  toggleActions: "restart none none reverse",
-  // onComplete: () => {
-  //   SplitType.revert(".char");
-  //   SplitType.revert(".split");
-  // },
-  // scrollTrigger: {
-  //   trigger: ".content",
-  //   start: "top center",
-  //   end: "bottom end",
-  //   markers: true,
-  //   scrub: 1,
-  //   onLeave: () => {
-  //     SplitType.revert(".char");
-  //     SplitType.revert(".split");
-  //   },
-  // },
-});
-gsap.utils.toArray(".block").forEach((block, i) => {
-  tl.from(block, {
-    scrollTrigger: {
-      trigger: block,
-      start: "top top",
-      pin: true,
-      scrub: 1,
-      markers: false,
-      onLeave: () => {
-        SplitType.revert(".char");
-        SplitType.revert(".split");
-      },
-    },
-  });
-});
+// tl.fromTo(
+//   ".intro-text",
+//   { opacity: 0 },
+//   { opacity: 1, delay: 0, duration: 2, stagger: 0.5, ease: "ease-in" }
+// ).from(chars2, {
+//   yPercent: 100,
+//   opacity: 0,
+//   stagger: 0.01,
+//   duration: 1,
+//   ease: "ease-in",
+//   toggleActions: "restart none none reverse",
+//   // onComplete: () => {
+//   //   SplitType.revert(".char");
+//   //   SplitType.revert(".split");
+//   // },
+//   // scrollTrigger: {
+//   //   trigger: ".content",
+//   //   start: "top center",
+//   //   end: "bottom end",
+//   //   markers: true,
+//   //   scrub: 1,
+//   //   onLeave: () => {
+//   //     SplitType.revert(".char");
+//   //     SplitType.revert(".split");
+//   //   },
+//   // },
+// });
+
+// tl.from("#block-1", {
+//   scrollTrigger: {
+//     trigger: "#block-1",
+//     start: "top top",
+//     pin: true,
+//     pinSpacing: false,
+//     scrub: 1,
+//     markers: false,
+//   },
+// });
+
+// gsap.utils.toArray(".block").forEach((block, i) => {
+//   tl.from(block, {
+//     scrollTrigger: {
+//       trigger: block,
+//       start: "top top",
+//       pin: false,
+//       scrub: 1,
+//       markers: false,
+//       onLeave: () => {
+//         SplitType.revert(".char");
+//         SplitType.revert(".split");
+//       },
+//     },
+//   });
+// });
 mm.add("(max-width: 768px)", () => {
   gsap.utils.toArray(".op").forEach((op, i) => {
     tl.from(op, {
--- src/scripts/services.js
+++ src/scripts/services.js
@@ -9,31 +9,51 @@ serviceNames.forEach((name) => {
   name.addEventListener("click", () => {
     let isOpen = false;
     let parentNode = name.parentNode;
+    let parentBox = parentNode.parentNode;
     let description = parentNode.querySelector(".service-desc");
     let icon = name.querySelector(".service-icon");
+
     description.classList.toggle("hidden");
     isOpen =
       !isOpen && !description.classList.contains("hidden") ? true : false;
     console.log(isOpen);
     icon.innerHTML = isOpen ? "south_east" : "north_east";
-    resetAll(description, icon);
 
-    // ! Da sistemare
+    resetAll(description, icon, parentBox);
+
     serviceLists.forEach((s) => {
-      if (isOpen) {
-        if (s != parentNode.parentNode) {
-          s.style.opacity = 0.5;
-          //   s.childNodes.forEach((c) => {
-          //     console.log(c);
-          //     if (c.hasChildNodes()) c.style.opacity = 0.5;
-          //   });
-        } else s.style.opacity = 1;
-      } else s.style.opacity = 1;
+      for (const c of s.children) {
+        if (isOpen) {
+          if (c != parentNode) {
+            c.style.opacity = 0.5;
+          } else c.style.opacity = 1;
+        } else c.style.opacity = 1;
+      }
     });
+    // ! Da sistemare
+    // serviceLists.forEach((s) => {
+    //   if (isOpen) {
+    //     if (s != parentNode.parentNode) {
+    //       s.style.opacity = 0.5;
+    //       //   s.childNodes.forEach((c) => {
+    //       //     console.log(c);
+    //       //     if (c.hasChildNodes()) c.style.opacity = 0.5;
+    //       //   });
+    //     } else s.style.opacity = 1;
+    //     // for (const c of parentBox.children) {
+    //     //   console.log(c);
+    //     //   if (isOpen) {
+    //     //     if (c != parentNode) {
+    //     //       c.style.opacity = 0.5;
+    //     //     } else c.style.opacity = 1;
+    //     //   } else c.style.opacity = 1;
+    //     // }
+    //   } else s.style.opacity = 1;
+    // });
   });
 });
 
-const resetAll = (d, i) => {
+const resetAll = (d, i, p) => {
   console.log("Hide class");
   serviceDescs.forEach((desc) => {
     if (!desc.classList.contains("hidden") && desc !== d) {
@@ -45,4 +65,9 @@ const resetAll = (d, i) => {
       icon.innerHTML = "north_east";
     }
   });
+  for (const c of p.children) {
+    if (c.style.opacity === 0.5 && c !== parentNode) {
+      c.style.opacity = 1;
+    }
+  }
 };
