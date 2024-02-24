c244c55bab161fc7b29883155d42850125015ff0
Alessio Brioschi <77493369+Brioche23@users.noreply.github.com>
2024-02-24 10:54:33 +0200

    Added Contentful link

---
+++ src/lib/contentful.ts
@@ -0,0 +1,20 @@
+import contentful from "contentful";
+import type { EntryFieldTypes } from "contentful";
+
+export interface Video {
+  contentTypeId: "video";
+  fields: {
+    title: EntryFieldTypes.Text;
+    slug: EntryFieldTypes.Text;
+    video: EntryFieldTypes.Object;
+    description: EntryFieldTypes.RichText;
+  };
+}
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
